b5f1d3889ecb316ed9a9c8a6e02b6a39ae62ebfe
Shabeeb R P <shabeebrp@gmail.com>
2020-05-04 14:26:20 +0530

    Create day_2_jewels_and_stones_771.java

---
+++ day_2_jewels_and_stones_771.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numJewelsInStones(String J, String S) {
+        Set<Character> jewels = new HashSet<>();
+        for(char ch : J.toCharArray())
+            jewels.add(ch);
+        int count = 0;
+        for(char ch : S.toCharArray()){
+            if(jewels.contains(ch))
+                count++;
+        }
+        return count;
+    }
+}
